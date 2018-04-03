2e38ed771800515cf200a0c770156bbd6439d928
julianbarnes <julian.barnes100@gmail.com>
2018-04-03 13:00:32 -0400

    Julian's UI updates

---
+++ src/components/dashboard/AssignmentItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import '../../styles/index.css';
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+
+export default class AssignmentItem extends React.Component {
+  render(){
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6">
+            {this.props.assignment.dueDate}
+          </div>
+          <div class="col-lg-6" align="right">
+            <Link to="/submit">
+              <p>{this.props.assignment.title}</p>
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- src/components/views/Dashboard.js
+++ src/components/views/Dashboard.js
@@ -1,13 +1,80 @@
-import React, { Component } from 'react';
 
-class Dashboard extends Component {
-    render() {
-        return (
-            <div className="Dashboard">
-                <h1>This page is protected</h1>
-            </div>
-        );
+/*import React from 'react';
+
+const Home = () => (
+    <div>
+        <h1>Hello, Welcome to SWAPR 2211</h1>
+    </div>
+);
+
+export default Home;
+*/
+import React from 'react';
+import ToDoAssignments from '../components/dashboard/ToDoAssignments';
+import CompletedAssignments from '../components/dashboard/CompletedAssignments';
+import '../../stylesindex.css';
+
+class Dashboard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todolist:[],
+      completedlist:[],
     }
+  }
+
+  getTodo() {
+    //Ajax api call
+    return;
+  }
+
+  getCompleted() {
+    //get
+    return;
+  }
+
+
+  componentWillMount() {
+    //this will call the getter methods
+    //getTodo();
+    //getCompleted();
+  }
+
+  render() {
+    return(
+        <div className="main-page container">
+          <div className="row">
+            <div className="col-lg-3">
+                <SideMenu/>
+            </div>
+            <div className="col-lg-9">
+              <div className="row justify-content-md-center" id="header"><h1 id="dashboard-title">Dashboard</h1></div>
+              <div className = "row justify-content-md-center">
+                <ToDoAssignments/>
+                <CompletedAssignments/>
+              </div>
+            </div>
+          </div>
+        </div>
+    )
+  }
 }
+function SideMenu(props) {
+  return(
+    <div className="side-menu">
+      <nav class="navbar">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a className="navbar-brand" id="title">SWAPR</a>
+          </li>
+          <li class="nav-item">
+            <a href="">Dashboard</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
 
 export default Dashboard;
